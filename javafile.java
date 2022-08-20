ad6d7aa791e306fb3c2428b6ecbb9cae4a33e39b
naveen030102 <88428599+naveen030102@users.noreply.github.com>
2022-08-20 18:12:58 +0530

    Update javafile.java

--- javafile.java
+++ javafile.java
@@ -3,6 +3,7 @@ public class javafile
 public static void main(String [] args)
 {
 System.out.println("IT WORKING PERFECTLY FINE Y");
+System.out.println("just checking the fecth is working");
 
 }
 }
